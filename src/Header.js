d44fd888d4ca5d033852c765efd811d87f9a1bcf
amitshah588 <anticamit@gmail.com>
2021-10-27 10:44:56 +0530

    amitshah

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div>
+            <div>Logo</div>
+            <div>This is my 1st React practise</div>
+            <button>YEs</button>
+            <button>No</button>
+        </div>
+    )
+}
+
+export default Header;
